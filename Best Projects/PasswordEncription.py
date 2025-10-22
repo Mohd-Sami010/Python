1cf2e13841c40f9753c1f6e9e82e2a04eaca2098
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2025-10-22 18:31:01 +0530

    Updated Encrypter

--- Best Projects/PasswordEncription.py
+++ Best Projects/PasswordEncription.py
@@ -1,14 +1,21 @@
 import random
-SEPRATOR = ";1"
+
+SEPRATOR = ";0"
+
+# Functions
 def Encrypt(textToEncrypt):
     encryptedText = ""
     for c in textToEncrypt:
-        # encryptedText+= c.
         numberToAdd = random.randint(0, 9)
         newChar = f"{numberToAdd}" + chr(ord(c) + numberToAdd) + SEPRATOR
         encryptedText += newChar
     return "[" + encryptedText + "]"
+
 def Decrypt(encryptedText):
+    encryptedText= encryptedText.strip()
+    if encryptedText[0] != "[" or encryptedText[-1] != "]":
+        print("Enter a valid Encryted Text with []")
+        return
     encryptedText = encryptedText[1:-2]
     actualText = ""
     for i in encryptedText.split(SEPRATOR):
@@ -18,22 +25,17 @@ def Decrypt(encryptedText):
         actualChar = chr((ord(i[1]) - numToSubtract))
         actualText += actualChar
     return actualText
+
+# Main Program
 print("\n\nWELCOME to PASYPTER\n")
 print("-----------------------------------------------------------------")
 
-print('''What do you want to do?
-1. Encrypt
-2. Decrypt''')
-task = input("Enter the option number: ")
-if task == "q":
-    exit()
-# textToEncrypt = "MohdSami" #2O10y0h3g2U0a7t6o
-# print(Encrypt(textToEncrypt))
+print("What do you want to do?\n1. Encrypt\n2. Decrypt")
 
+task = input("Enter the option number: ")
 if task == "1":
     textToEncrypt = input("Enter the text to Encrypt: ")
     print("Encrypted text:", Encrypt(textToEncrypt))
 elif task == "2":
     textToDecrypt = input("Enter the text to Decrypt: ")
     print("Actual text:", Decrypt(textToDecrypt))
-
