06dde5adca0ec99c1e50e49d3bd1fae0ad1f1d1a
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2026-02-20 14:30:40 +0530

    Practical1 Done

---
+++ Lab/01_ConvertToDec.py
@@ -0,0 +1,11 @@
+# Binary
+bin = input("Enter a binary number: ")
+print("Dec of", bin, "is", int(bin, 2))
+
+# Octal
+oct = input("Enter an octal number:")
+print("Dec of", oct, "is", int(oct, 8))
+
+# Hexadecimal
+hex = input("Enter a Hexadecimal number:")
+print("Dec of", hex, "is", int(hex, 16))
