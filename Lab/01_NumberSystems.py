2d8d7e3a40a3e26c588ccf7f984095de156f2138
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2026-02-25 23:16:00 +0530

    File Done

--- Lab/01_NumberSystems.py
+++ Lab/01_NumberSystems.py
@@ -4,25 +4,25 @@
 # and convert it to its equivalent binary, octal, and hexadecimal string.
 
 # Binary
-bin = input("Enter a binary number: ")
-print("Dec of", bin, "is", int(bin, 2))
+binNum = input("Enter a binary number: ")
+print("Dec of", binNum, "is", int(binNum, 2))
 
 # Octal
-oct = input("Enter an octal number:")
-print("Dec of", oct, "is", int(oct, 8))
+octNum = input("Enter an octal number:")
+print("Dec of", octNum, "is", int(octNum, 8))
 
 # Hexadecimal
-hex = input("Enter a Hexadecimal number:")
-print("Dec of", hex, "is", int(hex, 16))
+hexNum = input("Enter a Hexadecimal number:")
+print("Dec of", hexNum, "is", int(hexNum, 16))
 
 
 # Int to Other sys
 num = int(input("Enter a number: "))
 
-bin = bin(num)
-oct = oct(num)
-hex = hex(num)
+binNum = bin(num)
+octNum = oct(num)
+hexNum = hex(num)
 
-print(f"The binary value of {num} is {bin}")
-print(f"The octal value of {num} is {oct}")
-print(f"The hexadecimal value of {num} is {hex}")
+print(f"The binary value of {num} is {binNum}")
+print(f"The octal value of {num} is {octNum}")
+print(f"The hexadecimal value of {num} is {hexNum}")
